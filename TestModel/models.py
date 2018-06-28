cce3d048196a8b464f54b9a2ce669cbfa98c417a
huangzhi <huangczh109@126.com>
2018-06-28 17:30:43 +0800

    model
    model

--- TestModel/models.py
+++ TestModel/models.py
@@ -2,10 +2,16 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
-
+import datetime
+import time
 class userinformation(models.Model):
         id = models.AutoField(max_length=11,db_column='UID',primary_key=True)
         userName = models.CharField(max_length=50,db_column='userName',blank=False)
         passWord = models.CharField(max_length=25,db_column='passWord',blank=False)
         mobile = models.CharField(max_length=11,db_column='mobile',blank=True)
         sex = models.CharField(max_length=10,db_column='sex',blank=True)
+        createTime = models.DateTimeField(auto_now=True, null=True)
+        updateTime = models.DateTimeField(auto_now=True, null=True)
+
+
+
